7a6d170bee973205733ecb2d4fcede1d3917c859
evastakava <125592045+evastakava@users.noreply.github.com>
2023-04-16 19:04:18 +0000

    TEST

--- hash_substring.py
+++ hash_substring.py
@@ -3,7 +3,15 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    input_type = input().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'F':
+        with open(input().rstrip()) as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
+    return pattern, text
     
     # after input type choice
     # read two lines 
@@ -13,7 +21,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,9 +29,41 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    p = 10**9 + 7
+    x = 263
+    occurrences = []
+    m = len(pattern)
+    n = len(text)
+    if n < m:
+        return occurrences
+    p_hash = hash_string(pattern, p, x)
+    H = precompute_hashes(text, m, p, x)
+    for i in range (n - m + 1):
+        if p_hash != H[i]:
+            continue
+        if pattern == text[i:i+m]:
+            occurrences.append(i)
     # and return an iterable variable
-    return [0]
+    return occurrences
+
+def hash_string(s, p, x):
+    h = 0
+    for c in reversed(s):
+        h = (h * x + ord(c)) % p
+    return h
+
+def precompute_hashes(text, m, p, x):
+    n = len(text)
+    H = [0] * (n - m + 1)
+    s = text[n-m:]
+    H[n-m] = hash_string(s, p, x)
+    y = 1
+    for i in range(m):
+        y = (y * x) % p
+    for i in range(n - m - 1, -1, -1):
+        prehash = x * H[i+1] + ord(text[i] - y * ord(text[i+m]))
+        H[i] = prehash % p
+    return H
 
 
 # this part launches the functions
