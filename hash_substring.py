48384ffab8198166cf34614ad173df49e8a0a98c
evastakava <125592045+evastakava@users.noreply.github.com>
2023-04-16 19:12:19 +0000

    test

--- hash_substring.py
+++ hash_substring.py
@@ -15,16 +15,16 @@ def read_input():
    # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     
-    
+
     # after input type choice
     # read two lines 
     # first line is pattern 
     # second line is text in which to look for pattern 
     
     # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
 
+
+    
 
 def print_occurrences(output):
 
